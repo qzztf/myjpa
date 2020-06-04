10852fd54461619018e9a1c5cce3aaa647f4dfc9
qzz <qinzaizhen@ddjf.com.cn>
2020-06-04 18:33:50 +0800

    测试用例

--- myjpa-samples/samples-myjpa/src/main/java/cn/sexycode/myjpa/samples/Demo.java
+++ myjpa-samples/samples-myjpa/src/main/java/cn/sexycode/myjpa/samples/Demo.java
@@ -1,15 +1,11 @@
 package cn.sexycode.myjpa.samples;
 
 import cn.sexycode.myjpa.samples.model.User;
-import cn.sexycode.myjpa.session.SessionFactoryBuilderImpl;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * @author qinzaizhen
@@ -20,18 +16,22 @@ public class Demo {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
         User user = new User();
-        user.setId("2");
-        user.setAccount("qq");
+        user.setId("8");
+        user.setAccount("ffdfdfddd");
         user.setPassword("1111");
         user.setFullName("qzz");
         //以上两行为新建状态
         //托管状态
         em.persist(user);
-        //事务提交或调用flush()方法后会同步到数据库
-        em.getTransaction().commit();
+
         //根据主键获取对象
         System.err.println(em.find(User.class,1));
         //System.err.println(em.getReference(User.class,1));
+
+        user.setFullName("更新的值");
+        System.err.println(em.merge(user));
+        //事务提交或调用flush()方法后会同步到数据库
+        em.getTransaction().commit();
         em.close();
         entityManagerFactory.close();
     }
